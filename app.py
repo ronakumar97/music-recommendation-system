55208261292965160a3db59bdd7db700fb6fb8f4
Aniruddh Sodha <sodhaaniruddh843@gmail.com>
2021-11-03 18:45:15 -0400

    Project Set-up

---
+++ app.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template, request, url_for, redirect, session
+import pymongo
+import requests
+from datetime import datetime
+import processing
+import webbrowser
+
+app = Flask(__name__)
+app.secret_key = "AI-PROJECT"
+client = pymongo.MongoClient()
+
+db = client.get_database('music_project')
+
+user_records = db.users
+user_preferences = db.preferences
+user_history = db.history
+user_recommendations = db.recommendations #TODO;
+
+BEARER_TOKEN = 'BQA3WmdMYiuZl8qs6VJnxuiP4p-8wq2Y_WeSYmwQxR0O8wCLrOrjppDwC93woGJCwGgJuGUiI9L9j2gd_N4'
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method == "POST":
+        return processing.login(request)
+
+    return render_template('login.html', message=message)
+
+@app.route("/register", methods=['GET', 'POST'])
+def index():
+    if "email" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method == "POST":
+        return processing.register(request)
+
+    return render_template('index.html')
+
+@app.route("/register/preferences", methods=['GET', 'POST'])
+def save_preferences():
+    if request.method == "POST":
+        processing.save_preferences(request)
+
+    return render_template('logged_in.html')
+
+@app.route('/logged_in')
+def logged_in():
+    if "email" in session:
+        email = session["email"]
+        return render_template('logged_in.html', email=email)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("logout.html")
+    else:
+        return render_template('index.html')
+
+@app.route("/search_music", methods=["GET", "POST"])
+def search_music():
+    if(request.method == "POST"):
+        song_name = request.form.get('song_name')
+        user_found = user_records.find_one({"email": session['email']})
+        user_id = user_found['_id']
+        last_song_played_information = {'user_id': user_id, 'last_song_played': song_name, 'timestamp': datetime.now()}
+        user_history.insert_one(last_song_played_information)
+
+        search_url = 'https://api.spotify.com/v1/search'
+
+        params = {
+            'q': song_name,
+            'type': 'track',
+            'limit': 9,
+            'market': 'US'
+        }
+
+        headers = {"Authorization": "Bearer " + get_bearer_token()}
+
+        r = requests.get(search_url, params=params, headers=headers)
+        items = r.json()['tracks']['items']
+
+        songs = processing.fetch_songs(items)
+
+    return render_template('logged_in.html', songs = songs)
+
+def get_bearer_token():
+    token_url = 'https://accounts.spotify.com/api/token'
+    params = {
+        'grant_type': 'client_credentials'
+    }
+
+    headers = {"Authorization": "Basic MjlkOTFkYWU4NGFkNGVkOGI5M2E4MzRiMGNkZDA5YjQ6OTAwNGNmM2E0YzZkNDVmYmE5OGQ0MTM4ZDk5ZDE4NjE=",
+               "Content-Type" : "application/x-www-form-urlencoded"}
+    r = requests.post(token_url, params=params, headers=headers)
+    token = r.json()['access_token']
+    return token
+
+@app.route("/store_features", methods=["GET", "POST"])
+def store_features():
+    print("i am here")
+    if (request.method == "POST"):
+        webbrowser.open(request.form.get('preview_url'))
+
+        feature_url = 'https://api.spotify.com/v1/audio-features/'+ request.form.get('song_id')
+        headers = {"Authorization": "Bearer " + get_bearer_token()}
+        r = requests.get(feature_url, headers=headers)
+        processing.save_features(r.json())
+
+    #return render_template('logged_in.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ processing.py
@@ -0,0 +1,137 @@
+from flask import render_template, url_for, redirect, session
+import pymongo
+import bcrypt
+
+client = pymongo.MongoClient()
+
+db = client.get_database('music_project')
+
+user_records = db.users
+user_preferences = db.preferences
+user_history = db.history
+user_features = db.features
+user_recommendations = db.recommendations #TODO;
+
+def fetch_songs(items):
+    songs = []
+
+    for item in items:
+        album_id = item['album']['id']
+        album_name = item['album']['name']
+        album_number_of_tracks = item['album']['total_tracks']
+        album_image_url = item['album']['images'][0]['url']
+
+        preview_url = item['preview_url']
+        song_name = item['name']
+        is_explicit = item['explicit']
+        song_popularity = item['popularity']
+        song_id = item['id']
+
+        artist_id = item['artists'][0]['id']
+        artist_name = item['artists'][0]['name']
+
+        song_data = {
+            'album_id': album_id,
+            'album_name': album_name,
+            'album_number_of_tracks': album_number_of_tracks,
+            'album_image_url': album_image_url,
+            'preview_url': preview_url,
+            'song_name': song_name,
+            'is_explicit': is_explicit,
+            'song_popularity': song_popularity,
+            'artist_id': artist_id,
+            'artist_name': artist_name,
+            'song_id': song_id
+        }
+
+        songs.append(song_data)
+
+    return songs
+
+def login(request):
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    email_found = user_records.find_one({"email": email})
+
+    if email_found:
+        email_val = email_found['email']
+        password_check = email_found['password']
+
+        if bcrypt.checkpw(password.encode('utf-8'), password_check):
+            session["email"] = email_val
+            return redirect(url_for('logged_in'))
+        else:
+            if "email" in session:
+                return redirect(url_for("logged_in"))
+            message = 'Wrong password'
+            return render_template('login.html', message=message)
+    else:
+        message = 'Email not found'
+        return render_template('login.html', message=message)
+
+def register(request):
+    user = request.form.get("fullname")
+    email = request.form.get("email")
+    phone_number = request.form.get("phone_number")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+
+    user_found = user_records.find_one({"name": user})
+    email_found = user_records.find_one({"email": email})
+
+    if user_found:
+        message = 'There is already a user by that name'
+        return render_template('index.html', message=message)
+    if email_found:
+        message = 'This email already exists in database'
+        return render_template('index.html', message=message)
+    if password1 != password2:
+        message = 'Passwords should match!!'
+        return render_template('index.html', message=message)
+    else:
+        hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+        user_input = {'name': user, 'email': email, 'phone_number': phone_number, 'password': hashed}
+        user_records.insert_one(user_input)
+        user_data = user_records.find_one({"email": email})
+        new_email = user_data['email']
+        session['email'] = new_email
+        return render_template('preferences.html', email=new_email)
+
+def save_preferences(request):
+    country = request.form.get('country')
+    genre = request.form.get('genre')
+    age = request.form.get('age')
+    location = request.form.get('location')
+    artist = request.form.get('artist')
+
+    user_found = user_records.find_one({"email": session['email']})
+    user_id = user_found['_id']
+    user_preferences_input = {'_id': user_id, 'country': country, 'genre': genre, 'age': age, 'location': location, 'artist': artist}
+    user_preferences.insert_one(user_preferences_input)
+
+def save_features(feaures):
+    user_found = user_records.find_one({"email": session['email']})
+    user_id = user_found['_id']
+
+    user_features_input = {
+        'user_id': user_id,
+        'danceability': feaures['danceability'],
+        'energy': feaures['energy'],
+        'key': feaures['key'],
+        'loudness': feaures['loudness'],
+        'mode': feaures['mode'],
+        'speechiness': feaures['speechiness'],
+        'acousticness': feaures['acousticness'],
+        'instrumentalness': feaures['instrumentalness'],
+        'liveness': feaures['liveness'],
+        'valence': feaures['valence'],
+        'tempo': feaures['tempo'],
+        'type': feaures['type'],
+        'song_id': feaures['id'],
+        'duration_ms': feaures['duration_ms'],
+        'time_signature': feaures['time_signature']
+    }
+    user_features.insert_one(user_features_input)
+
+
